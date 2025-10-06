0bed32a79a30a8016b261abe7efff6c7ade3826a
Marcos Gabriel <moliveirateixeira96@gmail.com>
2025-10-06 04:19:18 +0100

    feat: adding middlewares for team creation

--- src/routes/teamRoutes.ts
+++ src/routes/teamRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
 import { TeamController } from "@/controllers/teamController";
+import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
 const teamRoutes = Router()
 const teamController = new TeamController()
 
 teamRoutes.get("/", teamController.read)
-teamRoutes.post("/", teamController.create)
+teamRoutes.post("/",ensureAuthenticated , verifyUserAuthorization(['admin']),teamController.create)
 teamRoutes.patch("/", teamController.update)
 teamRoutes.delete("/", teamController.delete)
 
