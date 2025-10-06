05c4627a21fda7600e288247e7602e7aff22d67a
Marcos Gabriel Oliveira Teixeira <moliveira@unknown46d1e604640b.lan>
2025-10-06 02:04:34 +0100

    refactor: removing example middleware from sessions routes, and user routes

--- src/routes/sessionsRoutes.ts
+++ src/routes/sessionsRoutes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 
 import { SessionsController } from "@/controllers/sessionsController";
-import { exampleMiddleware  } from "@/middlewares/exampleMiddleware";
 
 const sessionsRoutes = Router()
 const sessionsController = new SessionsController()
-
-sessionsRoutes.use(exampleMiddleware)
 
 sessionsRoutes.post("/", sessionsController.create)
 
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 
 import { UserController } from "@/controllers/userController";
-import { exampleMiddleware  } from "@/middlewares/exampleMiddleware";
 
 const userRoutes = Router()
 const userController = new UserController()
-
-userRoutes.use(exampleMiddleware)
 
 userRoutes.get("/", userController.read)
 userRoutes.post("/", userController.create)
