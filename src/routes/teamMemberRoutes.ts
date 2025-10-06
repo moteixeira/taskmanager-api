71937e7cd4a14ead2265b29068f780d095c4e4f2
Marcos Gabriel <moliveirateixeira96@gmail.com>
2025-10-06 07:07:23 +0100

    refactor: Adjusting the verb for create route

--- src/routes/teamMemberRoutes.ts
+++ src/routes/teamMemberRoutes.ts
@@ -7,7 +7,7 @@ import { TeamMemberController } from "@/controllers/teamMemberController";
 const teamMemberRoutes = Router()
 const teamMemberController = new TeamMemberController()
 
-teamMemberRoutes.put("/:team_id",ensureAuthenticated, verifyUserAuthorization(['admin']), teamMemberController.create)
+teamMemberRoutes.post("/:team_id",ensureAuthenticated, verifyUserAuthorization(['admin']), teamMemberController.create)
 teamMemberRoutes.delete("/:team_id",ensureAuthenticated, verifyUserAuthorization(['admin']), teamMemberController.delete)
 
 export { teamMemberRoutes }
